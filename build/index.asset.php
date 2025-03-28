e7844ad09389e54a78584303fb889be28d2f12ea
Pavenest <pavenest@gmail.com>
2025-03-28 17:36:22 +0600

    #updated after asset build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => 'aa9d04b67f67dbbc2697');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '5b0e2c1767124255a03a');
